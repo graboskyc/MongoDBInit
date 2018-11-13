d69b5a368931238510f6b60a574004776c4c8ce6
Chris Grabosky <chris@grabosky.net>
2018-11-13 14:44:00 -0500

    Update StageMyShit.sh

--- StageMyShit.sh
+++ StageMyShit.sh
@@ -6,3 +6,4 @@ wget https://raw.githubusercontent.com/graboskyc/MongoDBInit/master/installEA.sh
 wget https://raw.githubusercontent.com/graboskyc/MongoDBInit/master/makeRSConfs.sh -O makeRSConfs.sh
 wget https://raw.githubusercontent.com/graboskyc/MongoDBInit/master/runRS.sh -O runRS.sh
 wget https://raw.githubusercontent.com/graboskyc/MongoDBInit/master/installAutoAgent.sh -O installAutoAgent.sh
+chmod +x ./*.sh
