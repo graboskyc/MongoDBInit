13a177fc20700d217e504cd391315e0be4eec77f
Chris Grabosky <chris@grabosky.net>
2020-02-18 09:33:54 -0500

    Update StageMyShit.sh

--- StageMyShit.sh
+++ StageMyShit.sh
@@ -8,4 +8,5 @@ wget https://raw.githubusercontent.com/graboskyc/MongoDBInit/master/installEA.sh
 wget https://raw.githubusercontent.com/graboskyc/MongoDBInit/master/makeRSConfs.sh -O makeRSConfs.sh
 wget https://raw.githubusercontent.com/graboskyc/MongoDBInit/master/runRS.sh -O runRS.sh
 wget https://raw.githubusercontent.com/graboskyc/MongoDBInit/master/installAutoAgent.sh -O installAutoAgent.sh
+wget https://raw.githubusercontent.com/graboskyc/MongoDBInit/master/installEAUbuntu.sh -O installEAUbuntu.sh
 chmod +x ./*.sh
